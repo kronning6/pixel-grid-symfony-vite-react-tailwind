edd75441c9d7745e4403f56c8c0b828e12767316
Kyle Ronning <kronning@kipsu.com>
2023-03-25 07:17:27 -0500

    Create full height dashboard and base structure for pixel grid

--- app/src/features/Dashboard/Dashboard.tsx
+++ app/src/features/Dashboard/Dashboard.tsx
@@ -4,6 +4,17 @@ import MenuItems from '../../components/MenuItems/MenuItems';
 import AppWrapper from '../../components/AppWrapper/AppWrapper';
 import AppBody from '../../components/AppBody/AppBody';
 
+const DashboardBody = (props: PropsWithChildren): JSX.Element => {
+  return (
+    <div className="flex h-full flex-col">
+      <div className="h-full overflow-hidden">{props.children}</div>
+      <div className="p-5 text-center font-extralight text-white">
+        A Kyle Ronning Production
+      </div>
+    </div>
+  );
+};
+
 const Dashboard = (props: PropsWithChildren): JSX.Element => {
   return (
     <AppWrapper>
@@ -16,7 +27,9 @@ const Dashboard = (props: PropsWithChildren): JSX.Element => {
           ]}
         />
       </AppBar>
-      <AppBody>{props.children}</AppBody>
+      <AppBody>
+        <DashboardBody>{props.children}</DashboardBody>
+      </AppBody>
     </AppWrapper>
   );
 };
